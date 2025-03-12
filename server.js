22f3fc5f2ca7ca65b2711b28407c4bbf432c3158
johnsonfash <fashanutosin7@gmail.com>
2025-03-12 07:00:04 +0100

    a

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import { readFileSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import fs from 'fs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const indexHtmlPath = path.join(__dirname, "dist/index.html");
+
+function getFilesAndFoldersSync(directory) {
+    try {
+        return fs.readdirSync(directory).map(name => {
+            const fullPath = path.join(directory, name);
+            const isDirectory = fs.statSync(fullPath).isDirectory();
+            return { name, fullPath, isDirectory };
+        });
+    } catch (err) {
+        console.error(`Error reading folder: ${err.message}`);
+        return []; // Return empty array if an error occurs
+    }
+}
 
 export default function handler(req, res) {
     try {
@@ -12,6 +25,6 @@ export default function handler(req, res) {
         res.end(html);
     } catch (err) {
         res.statusCode = 500;
-        res.end(`Error: index.html not found ${__dirname}`);
+        res.end(`Error: index.html not found ${getFilesAndFoldersSync(__dirname).join(', ')}`);
     }
 }
