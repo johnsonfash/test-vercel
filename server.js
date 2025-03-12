231a4756d3f0be61a2b03fe117b35738567ebd61
johnsonfash <fashanutosin7@gmail.com>
2025-03-12 08:01:29 +0100

    a

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ import { fileURLToPath } from 'url';
 import serverless from 'serverless-http';
 
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname, 'dist')));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.send('hello world');
+    // res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 
 const isVercel = process.env.VERCEL === '1';
 if (isVercel) {
