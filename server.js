db772002217a2cd0f79d5e7b72ec25133f5c3a4f
johnsonfash <fashanutosin7@gmail.com>
2025-03-12 07:12:47 +0100

    a

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 const isVercel = process.env.VERCEL === '1';
