dcb5edf148a392a4b5a829828a09d38352c99bb0
johnsonfash <fashanutosin7@gmail.com>
2025-03-12 06:51:36 +0100

    a

---
+++ server copy.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import serverless from 'serverless-http';
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
+const isVercel = process.env.VERCEL === '1';
+if (isVercel) {
+    module.exports = app;
+    module.exports.handler = serverless(app);
+} else {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
--- server.js
+++ server.js
@@ -1,25 +1,17 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import serverless from 'serverless-http';
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const indexHtmlPath = path.join(__dirname, "dist/index.html");
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-
-const isVercel = process.env.VERCEL === '1';
-if (isVercel) {
-    module.exports = app;
-    module.exports.handler = serverless(app);
-} else {
-    const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+export default function handler(req, res) {
+    try {
+        const html = readFileSync(indexHtmlPath, "utf-8");
+        res.setHeader("Content-Type", "text/html");
+        res.end(html);
+    } catch (err) {
+        res.statusCode = 500;
+        res.end("Error: index.html not found");
+    }
 }
